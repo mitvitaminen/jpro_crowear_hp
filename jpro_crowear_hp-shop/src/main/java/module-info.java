45796799a3aa984520876791ebc36bed0e4c519d
Christian Rocholl <chris.rocholl@gmail.com>
2023-05-22 06:18:05 +0200

    module-info sorted

--- jpro_crowear_hp-shop/src/main/java/module-info.java
+++ jpro_crowear_hp-shop/src/main/java/module-info.java
@@ -1,19 +1,19 @@
 module net.crowear.shop {
+   requires com.google.common;
+   requires com.google.guice;
+   requires com.google.guice.extensions.persist;
+   requires com.h2database;
    requires de.mkammerer.argon2.nolibs;
    requires de.saxsys.mvvmfx;
    requires de.saxsys.mvvmfx.guice;
    requires de.saxsys.mvvmfx.validation;
-   requires com.h2database;
    requires jakarta.inject;
    requires jakarta.persistence;
-   requires shiro.core;
-   requires shiro.guice;
    requires java.sql;
    requires javafx.graphics;
    requires javafx.fxml;
-   requires org.apache.logging.log4j;
    requires one.jpro.routing.core;
-   requires com.google.common;
-   requires com.google.guice;
-   requires com.google.guice.extensions.persist;
+   requires org.apache.logging.log4j;
+   requires shiro.core;
+   requires shiro.guice;
 }
