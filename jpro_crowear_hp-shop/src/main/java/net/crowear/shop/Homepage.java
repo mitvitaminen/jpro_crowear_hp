762b3b06a2422ae3bbb75ebc58753b9d463b356a
Christian Rocholl <chris.rocholl@gmail.com>
2023-06-18 16:49:33 +0200

    removed route

--- jpro_crowear_hp-shop/src/main/java/net/crowear/shop/Homepage.java
+++ jpro_crowear_hp-shop/src/main/java/net/crowear/shop/Homepage.java
@@ -11,7 +11,6 @@ import de.saxsys.mvvmfx.FluentViewLoader;
 import de.saxsys.mvvmfx.ViewTuple;
 import de.saxsys.mvvmfx.guice.MvvmfxGuiceApplication;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import net.crowear.shop.mod.DataSourceModule;
 import net.crowear.shop.mod.EventBusModule;
@@ -24,13 +23,7 @@ import net.crowear.shop.ui.dialog.error.ErrorDialogView;
 import net.crowear.shop.ui.dialog.error.ErrorDialogViewModel;
 import net.crowear.shop.ui.page.index.IndexPageView;
 import net.crowear.shop.ui.page.index.IndexPageViewModel;
-import net.crowear.shop.ui.page.memberarea.MemberAreaPageView;
-import net.crowear.shop.ui.page.memberarea.MemberAreaPageViewModel;
 import net.crowear.shop.ui.util.DialogHelper;
-import one.jpro.routing.Route;
-import one.jpro.routing.RouteNode;
-import one.jpro.routing.RouteUtils;
-import one.jpro.routing.sessionmanager.SessionManager;
 
 public class Homepage extends MvvmfxGuiceApplication {
 
@@ -55,18 +48,14 @@ public class Homepage extends MvvmfxGuiceApplication {
    @Override
    public void startMvvmfx(final Stage stage) throws Exception {
       try {
-         final RouteNode routeNode = new RouteNode(stage);
 
-         // Add node between RouteNode and Scene, so Popups work correctly with
-         // ScenicView
-         final StackPane root = new StackPane(routeNode);
+         final ViewTuple<IndexPageView, IndexPageViewModel> indexPageViewTuple = FluentViewLoader
+               .fxmlView(IndexPageView.class).load();
 
-         final Scene scene = new Scene(root);
+         final Scene scene = new Scene(indexPageViewTuple.getView());
          scene.getStylesheets().addAll("css-homepage.css");
          stage.setScene(scene);
-         routeNode.setRoute(createRoute());
          stage.show();
-         routeNode.start(SessionManager.getDefault(routeNode, stage));
       } catch (final Exception e) {
          final ViewTuple<ErrorDialogView, ErrorDialogViewModel> viewTuple = FluentViewLoader
                .fxmlView(ErrorDialogView.class).load();
@@ -77,20 +66,6 @@ public class Homepage extends MvvmfxGuiceApplication {
 
          throw e;
       }
-   }
-
-   private Route createRoute() {
-      final ViewTuple<IndexPageView, IndexPageViewModel> indexPageViewTuple = FluentViewLoader
-            .fxmlView(IndexPageView.class).load();
-      indexPageViewTuple.getCodeBehind().setContent(indexPageViewTuple.getView());
-
-      final ViewTuple<MemberAreaPageView, MemberAreaPageViewModel> memberAreaPageViewTuple = FluentViewLoader
-            .fxmlView(MemberAreaPageView.class).load();
-      memberAreaPageViewTuple.getCodeBehind().setContent(memberAreaPageViewTuple.getView());
-
-      return Route.empty().and(RouteUtils.redirect("/", "/crowearindex"))
-            .and(RouteUtils.getNode("/crowearindex", (s) -> indexPageViewTuple.getView()))
-            .and(RouteUtils.getNode("/memberarea", (s) -> memberAreaPageViewTuple.getView()));
    }
 
 }
