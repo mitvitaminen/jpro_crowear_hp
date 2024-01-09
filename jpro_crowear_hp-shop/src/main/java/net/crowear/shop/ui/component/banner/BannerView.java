58cb428ed444ac38716885acd1409fd661bda8fe
bextownpartys.de <bextownpartys@gmail.com>
2024-01-09 06:28:49 +0100

    changes

--- jpro_crowear_hp-shop/src/main/java/net/crowear/shop/ui/component/banner/BannerView.java
+++ jpro_crowear_hp-shop/src/main/java/net/crowear/shop/ui/component/banner/BannerView.java
@@ -1,12 +1,12 @@
 package net.crowear.shop.ui.component.banner;
 
-import net.crowear.shop.ui.BaseView;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import net.crowear.shop.ui.BaseView;
 
 public class BannerView extends BaseView<BannerViewModel> {
 
    @FXML
-   private ImageView imgCannabisfeldImageView;
+   private ImageView imgBackgroundImageView;
 
 }
